efea5775b6c11ecea62ffb681fd1f92b45b73c9c
dalramjwi <dalramjwi@gmail.com>
2024-09-09 11:15:33 +0900

    배열, 객체, 튜플

--- testOne.py
+++ testOne.py
@@ -8,3 +8,10 @@ print(string_merge())
 def example():
     return "ex"
 anther_ex = lambda: "ex2"
+arr = ["a","b","c"]
+obj = {
+  "dfd": "sdf",
+  "dfdf": "asd"
+}
+#기본 모양은 배열이지만, 중복을 사용할 수 없다.
+another_obj = ("gfea","agewr","gjh")
