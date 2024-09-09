d39c017729feb9a6d38a19cd058b06b41d9dc260
dalramjwi <dalramjwi@gmail.com>
2024-09-09 11:44:13 +0900

    단순한 방법

--- inout.py
+++ inout.py
@@ -1,4 +1,5 @@
+my_firstname = input("성 입력")
 my_name = input("이름 입력")
-def print_name(name):
-    print(f"내이름{name}")
-print_name(my_name)
+def print_name(my_firstname,my_name):
+    print(f"내 성은{my_firstname}이고, 내 이름은 {my_name}")
+print_name(my_firstname,my_name)
